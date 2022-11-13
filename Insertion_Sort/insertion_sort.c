569255f626523962b1d854dc6aa15361de1f6340
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-13 19:58:20 -0300

    Adding insertion sort for online judge.

--- Insertion_Sort/insertion_sort.c
+++ Insertion_Sort/insertion_sort.c
@@ -25,16 +25,22 @@ void printArray(int arr[], int array_size)
     printf("\n");
 }
 
-int main(){
-    int array[] = {19, 4, 5, 6, 20, 5, 3, 22};
+int main(){   
+    int array[999];
+    int cont = 0;
+
+    while (scanf("%d", &array[cont]) != EOF)
+    {
+        cont++;
+    }
 
     // This line is getting the size of the array. The idea behind is that the sizeof(array) will return the number of bytes in the array,
     // To transform this value to the size of the array only need to divide its value to the type of data that is being stored. In this case int, 
     // but could be char, float, etc.
-    int array_size = sizeof(array)/sizeof(int);
-
-    insertionSort(array, array_size);
-    printArray(array, array_size);
+    // int array_size = sizeof(array)/sizeof(int);
+    // printf("%d ", cont);
+    insertionSort(array, cont);
+    printArray(array, cont);
 
     return 0;
 }
